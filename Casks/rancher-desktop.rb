73ad2c0babb9b0013b794e9a98df0ac68da10b6a
Roman Zhuzha <sanyer@users.noreply.github.com>
2022-08-05 19:28:06 +0200

    updates for rancher-desktop
    get rid of livecheck and conflicts
    updates from upstream

--- Casks/rancher-desktop.rb
+++ Casks/rancher-desktop.rb
@@ -15,29 +15,21 @@ cask "rancher-desktop" do
   desc "Kubernetes and container management on the desktop"
   homepage "https://rancherdesktop.io/"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
-
   auto_updates true
-  conflicts_with formula: %w[
-    docker
-    helm
-    kubernetes-cli
-  ]
 
   app "Rancher Desktop.app"
 
   uninstall delete: [
               "/opt/rancher-desktop",
               "/private/etc/sudoers.d/zzzzz-rancher-desktop-lima", # zzzzz is not a typo
-              "/private/var/run/rancher-desktop-lima",
+              "/private/var/run/docker.sock",
+              "/private/var/run/rancher-desktop-*",
             ],
             quit:   "io.rancherdesktop.app"
 
   zap trash: [
     "~/.kuberlr",
+    "~/.rd",
     "~/Library/Application Support/Caches/rancher-desktop-updater",
     "~/Library/Application Support/Rancher Desktop",
     "~/Library/Application Support/rancher-desktop",
